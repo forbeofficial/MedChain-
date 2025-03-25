83dc72a3b92f19ffaa22f0ad5ed86bbee1adaf04
Harishankar.1 <88716735+Harishankar14@users.noreply.github.com>
2025-03-25 22:53:12 +0530

    Fixed db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        if (!process.env.MONGO_URI) {
+            throw new Error('MONGO_URI is not defined in the environment variables');
+        }
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
