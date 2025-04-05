05ad8a5d7db5457fbd58b53a0877911a1c90d88e
shivanandu <shivanandu.dev@gmail.com>
2025-04-05 21:05:16 +0530

    Delete OneDrive/Desktop/HackToFuture-main2/HackToFuture-main/backend directory

--- OneDrive/Desktop/HackToFuture-main2/HackToFuture-main/backend/routes/doctor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const doctorSchema = new mongoose.Schema({
-    name: String, 
-    password: String
-});
-
-const Doctor = mongoose.model("Doctor", doctorSchema);
-module.exports = Doctor;
