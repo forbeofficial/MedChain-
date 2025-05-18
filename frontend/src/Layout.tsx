11f2bd21fa521dcc182f041f557fe966a93df716
snk-git-hub <shivanandu.dev@gmail.com>
2025-05-18 17:05:49 +0530

    signin and signup tested and its working

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,10 +1,17 @@
 
 import { Outlet } from "react-router-dom"
 import { TopNavbar } from "./components/Navbar"
+import { useState } from "react"
 export function Layout(){
+
+    const[isLoggedIn,setIsLoggedIn]=useState(false);
+    useState(()=>{
+        const userToken = localStorage.getItem("token");
+        setIsLoggedIn(!!userToken);
+    },[]);
     return (
     <>
-    <TopNavbar/>
+    <TopNavbar isLoggedIn={isLoggedIn} />
     <Outlet/>
     </>
     )
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
 
-export function TopNavbar() {
+export function TopNavbar({ isLoggedIn }) {
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center">
       <div className="flex space-x-6 text-sm font-semibold">
     
         <Link to="/landing" className="hover:text-gray-300 transition">
           ABOUT US
-        </Link>
-        <Link to="/role" className="hover:text-gray-300 transition">
-          REGISTER
-        </Link>
-        <Link to="/signin" className="hover:text-gray-300 transition">
-          LOG IN
-        </Link>
+          </Link>
+          {!isLoggedIn && (
+          <>
+            <Link to="/role" className="hover:text-gray-300 transition">
+              REGISTER
+            </Link>
+            <Link to="/signin" className="hover:text-gray-300 transition">
+              LOG IN
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
